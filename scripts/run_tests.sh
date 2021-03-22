03d794d17515cc8c61ac273047f6edc5b75bb9db
Erik Pearson <eapearson@lbl.gov>
2021-03-22 08:17:03 -0700

    Restore running black, set paths specifically for all pre-test checks to prevent spamming virtualenvs.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,19 +6,24 @@ set -e
 (cd /app/relation_engine_server/test/spec_release && \
   tar czvf spec.tar.gz sample_spec_release)
 
-# TODO: why reformatting code?
-# black .
-flake8 --max-complexity 20 /app/relation_engine_server
-mypy --ignore-missing-imports /app/relation_engine_server
-bandit -r /app/relation_engine_server
+# Run code quality checks before tests; a failure here
+# will prevent the tests  from running.
+source_dirs="/app/relation_engine_server /app/client_src /app/spec"
+black $source_dirs
+flake8 --max-complexity 20 $source_dirs
+mypy --ignore-missing-imports $source_dirs
+bandit -r $source_dirs
 
 # start server, using the specs in /spec/repo
 sh /app/scripts/start_server.sh &
 coverage erase
+
 # spec validation
 python -m spec.validate
+
 # run importer/, relation_engine_server/, and spec/ tests
 coverage run --branch -m unittest discover -v
+
 # RE client tests
 PYTHONPATH=client_src python -m unittest discover client_src/test
 coverage html --omit=*/test_*
